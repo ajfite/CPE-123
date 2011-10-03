61cffd2b0bb60b8bed5cac681405d6255f031244
AJ Fite <austinjfite@hotmail.com>
2011-10-03 15:33:53 -0700

    Assignment 1 alpha (assignment is still non functional)

---
+++ assignment1.rkt
@@ -0,0 +1,24 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname assignment1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(require (planet "main.rkt" ("clements" "rsound.plt" 2 5)))
+
+;Define song and silence
+(define song "hg.wav")
+(define silence1 (silence 88200))
+
+;cut song
+(define clip1 (rs-read/clip song 123 123))
+
+;make song
+(define CompleteSong (rs-append clip1 silence1 clip2))
+
+;play
+(define (go) (play CompleteSong))
+
+;write
+(rs-write CompleteSong "Assignment1Song.wav")
+
+
+
+
