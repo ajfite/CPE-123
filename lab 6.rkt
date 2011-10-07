caf5b3f455e588d322e38ddc355d8ee9e07c6a4c
AJ Fite <austinjfite@hotmail.com>
2011-10-07 16:58:56 -0700

    Less Chuck Testa

--- lab 6.rkt
+++ lab 6.rkt
@@ -25,8 +25,8 @@
 )
 ;Classify
 (define (classify pitch1 pitch2)
-  (if (or (= (/ 2 3) (/ pitch1 pitch2)) (= (/ 3 2) (/ pitch1 pitch2))) "fifth" 
+  (if (or (= (/ 2 3) (/ pitch1 pitch2)) (= (/ 3 2) (/ pitch1 pitch2))) "fifth"
       (if (or (= (/ 5 4) (/ pitch1 pitch2)) (= (/ 4 5) (/ pitch1 pitch2))) "third" 
           (if (or (= (/ 6 5) (/ pitch1 pitch2)) (= (/ 5 6) (/ pitch1 pitch2))) "third"
-              "NOPE Chuck Testa"))) 
+              "Unknown"))) 
 )
