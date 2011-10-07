9689c327b6a702c8c7be1e6a307cbe069b280d8d
AJ Fite <austinjfite@hotmail.com>
2011-10-07 16:54:48 -0700

    Finished Classify, it appears to work

--- lab 6.rkt
+++ lab 6.rkt
@@ -23,3 +23,10 @@
                         (make-tone pitch2 1 44100)
                         (make-tone pitch3 1 44100))))
 )
+;Classify
+(define (classify pitch1 pitch2)
+  (if (or (= (/ 2 3) (/ pitch1 pitch2)) (= (/ 3 2) (/ pitch1 pitch2))) "fifth" 
+      (if (or (= (/ 5 4) (/ pitch1 pitch2)) (= (/ 4 5) (/ pitch1 pitch2))) "third" 
+          (if (or (= (/ 6 5) (/ pitch1 pitch2)) (= (/ 5 6) (/ pitch1 pitch2))) "third"
+              "NOPE Chuck Testa"))) 
+)
