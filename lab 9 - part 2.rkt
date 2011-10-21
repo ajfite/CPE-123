ab42bdeec166c62dc9057431237fde3f8537cca3
AJ Fite <austinjfite@hotmail.com>
2011-10-21 16:58:12 -0700

    All that is left to do is document the make a song function per lab guidelines

--- lab 9 - part 2.rkt
+++ lab 9 - part 2.rkt
@@ -1,4 +1,5 @@
 #lang Racket
+(require (planet "main.rkt" ("clements" "rsound.plt" 2 5)))
 
 ; From "Lab 9.rkt"
 (define (note n)
@@ -9,3 +10,11 @@
 ; Make a song
 ;; 
 ;;
+
+(define (notes->music notes)
+   (rs-append* 
+    (for/list ([notes (in-list notes)])
+      (note notes))))
+
+(play (notes->music (list 64 64 65 67 67 65 64 62 60 60 62 64 64 62 62 false 64 64 65 67 67 65 64 62 60 60 62 64 62 60 60)))
+
