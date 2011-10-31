c8de5322b563a85d9f6c51edfb677a9ac4b8ecef
AJ Fite <austinjfite@hotmail.com>
2011-10-30 19:53:21 -0700

    Conquered Reverse-odd-seconds :D

--- lab 10.rkt
+++ lab 10.rkt
@@ -13,8 +13,12 @@
 (rsound-draw (reverse-sound ding))
 
 ;;
-#;(define reverse-odd-seconds
-  ([local
-    
-  ])
+(define (reverse-odd-seconds sound)
+  (local [;current second = (floor (/ (rsound-frames sound) num))
+          (define (odd-seconds num) (cond [(> (modulo num 88200) 44100) (rs-ith/left sound num)]
+                                          [else (rs-ith/left sound (- (+ 44100 (- (* 44100  (floor (/ num 44100))) (modulo num 44100))) 1))]))]
+    (mono-signal->rsound (rsound-frames sound) odd-seconds)
+  )
 )
+(rsound-draw (reverse-odd-seconds (rs-read/clip "test.wav" 0 (* 44100 10))))
+(play (reverse-odd-seconds (rs-read/clip "test.wav" 0 (* 44100 10))))
