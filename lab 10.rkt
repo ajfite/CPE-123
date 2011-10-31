40cc8ccae1652b6b444d3d6a56f21d60eccf7f72
AJ Fite <austinjfite@hotmail.com>
2011-10-31 16:18:41 -0700

    Lab 10 part 3 *almost* working :D

--- lab 10.rkt
+++ lab 10.rkt
@@ -3,6 +3,7 @@
 (require (planet "main.rkt" ("clements" "rsound.plt" 2 7)))
 (require (planet "draw.rkt" ("clements" "rsound.plt" 2 7)))
 
+(define test-music (rs-read/clip "If I had $1000000.wav" 0 (* 44100 15)))
 
 ;; reverse-sound : rsound -> rsound
 ;; Reverse a sound
@@ -12,6 +13,7 @@
   )
 )
 ;Testing 1 2 3
+(rsound-draw ding)
 (rsound-draw (reverse-sound ding))
 
 
@@ -24,19 +26,20 @@
   )
 )
 ; Testing 1 2 3
-(rsound-draw (reverse-odd-seconds (rs-read/clip "If I had $1000000.wav" 0 (* 44100 15))))
-(play (reverse-odd-seconds (rs-read/clip "If I had $1000000.wav" 0 (* 44100 15))))
+(rsound-draw (reverse-odd-seconds test-music))
+;(play (reverse-odd-seconds test-music))
 
 
 ;; rescale : rsound -> rsound
 ;; takes in a sound and a number and returns a sound which is shrunk by the number
 (define (rescale sound renum)
-  (local [(define (make-a-sound num) (cond [() ()]
-                                           [else ()])
-         )]
-  )
+  (local [(define (make-a-sound num) (rs-ith/left sound (inexact->exact (* num renum))))]
+  (mono-signal->rsound (* (rsound-frames sound) renum) make-a-sound))
 )
-(rsound-draw (rescale (rs-read/clip "If I had $10000000.wav" 0 (* 44100 15)) .5))
+; Testing 1 2 3
+(rsound-draw (rescale test-music .5))
+(rsound-draw (rescale test-music 2))
+(play (rescale test-music .5))
 
 
 ;; The random number explosion of sound and terror
