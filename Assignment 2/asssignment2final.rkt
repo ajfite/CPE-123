2b37b07876d0483a188b3d5221de8d6855dfa203
AJ Fite <austinjfite@hotmail.com>
2011-11-11 18:30:23 -0800

    Final Final Final (Hopefully)

--- Assignment 2/asssignment2final.rkt
+++ Assignment 2/asssignment2final.rkt
@@ -129,12 +129,12 @@
 (define Verse1 (overlay* (list (scale .5 (vocal VocalVerse1))
                                (scale .2 (Guitar2 Guitar2Verse1)))))
 (define Chorus3 (scale .5 (vocal VocalChorus)))
-#;(define Verse2 (overlay* (list (scale .5 (vocal VocalVerse2))
-                                 (scale .2 (Guitar3 Guitar2Verse2)))))
+(define Verse2 (overlay* (list (scale .5 (vocal VocalVerse2))
+                               (scale .2 (Guitar3 Guitar2Verse2)))))
 (define Interlude (overlay* (list (scale .5 (vocal VocalInterlude))
                                   (scale .1 (Guitar3 Guitar3Interlude)))))
-#;(define Verse3 (overlay* (list (scale .5 (vocal VocalVerse3))
-                                 (scale .2 (Guitar2 Guitar2Verse3)))))
+(define Verse3 (overlay* (list (scale .5 (vocal VocalVerse3))
+                               (scale .2 (Guitar2 Guitar2Verse3)))))
 (define Outro (scale .5 (vocal VocalOutro)))
 
 (play (rs-append* (list Verse1
