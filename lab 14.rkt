532f0cf2b59802a34d8d262d6ca1ebf93720ce87
AJ Fite <austinjfite@hotmail.com>
2011-11-16 16:33:31 -0800

    Removed extra lines, formatting

--- lab 14.rkt
+++ lab 14.rkt
@@ -9,11 +9,10 @@
 (chord (list 65 66 67 68) 44100)
 
 ; a key-signature is 
-;(key-signature string string string string string string string)
+; (key-signature string string string string string string string)
 (define-struct key-signature (A B C D E F G))
 (key-signature "flat" "natural" "sharp" "natural" "flat" "sharp" "sharp")
 
 ; a chord-sequence is (make-chord-sequence list number)
 (define-struct chord-sequence (chord-list volume))
-(chord-sequence (list (chord 12 13) (chord 16 34)) 1) 
-
+(chord-sequence (list (chord 12 13) (chord 16 34)) 1)
